977c348f59b6d6120f6f155973a18e60d6e13b91
amdhanwate <iamdhanwate@gmail.com>
2023-03-24 12:46:15 +0530

    added experiences page

---
+++ src/components/Experiences/Experiences.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Particle from "../Particle";
+import ExperienceCard from "./ExperienceCard";
+
+import * as logos from "./experienceLogos";
+
+import "./experiences.css";
+
+function Experiences() {
+
+  const experiences = [
+    {
+      "title": "Product Engineer Intern",
+      "organization": "Kylas.io",
+      "organizationLogo": logos.kylasLogo,
+      "duration": "Feb 2023 - Present",
+      "description": [],
+      "skills": "Springboot, PostgreSQL, Java",
+      "experienceType": "Internship",
+    },
+    {
+      "title": "Web Development Intern",
+      "organization": "Q Blocks",
+      "organizationLogo": logos.qblocksLogo,
+      "duration": "Jun 2022 - July 2022",
+      "description": [],
+      "skills": "Software Development, Front-End Development,Git, User Interface Design, AJAX, Bootstrap, REST APIs, Python (Programming Language), JavaScript",
+      "experienceType": "Internship",
+    },
+    {
+      "title": "Frontend Web Developer Intern",
+      "organization": "Samagra Foundation",
+      "organizationLogo": logos.samagraLogo,
+      "duration": "Aug 2021 - Sep 2021",
+      "description": [
+        "Developed frontend for fundraising portal of the organization",
+        "Assisting and collaborating with the teammates to work on HTML, CSS, JS, Bootstrap, PHP and various APIs"
+      ],
+      "skills": "Front-End Development, PHP, MySQL, JavaScript, HTML5, Cascading Style Sheets (CSS)",
+      "experienceType": "Internship",
+    },
+    {
+      "title": "Python Developer Associate",
+      "organization": "TEN Labs",
+      "organizationLogo": logos.tenlabsLogo,
+      "duration": "May 2021 - Aug 2021",
+      "description": [
+        "Built an API using Django Rest Framework for the EdTech startup.",
+        "Scraped websites for course information and built production ready API",
+      ],
+      "skills": "Object-Oriented Programming (OOP), Flask, Django REST Framework, Django, REST APIs, Python (Programming Language), Web Development",
+      "experienceType": "Internship",
+    },
+    {
+      "title": "Web Developer",
+      "organization": "Mozilla Opensource Club, MITAOE",
+      "organizationLogo": logos.mozillaLogo,
+      "duration": "Oct 2020 - July 2021",
+      "description": [],
+      "skills": "Front-End Development",
+      "experienceType": "College Club",
+    },
+  ]
+
+  return (
+    <Container className="py-5 mt-5">
+      <Particle />
+      <div className="main-timeline">
+        {
+          experiences.map((experience, index) => {
+            return (
+              <div className={`timeline ${index % 2 === 0 ? "left" : "right"}`}>
+                <ExperienceCard title={experience.title} duration={experience.duration} organization={experience.organization} description={experience.description} experienceType={experience.experienceType} skills={experience.skills} organizationLogo={experience.organizationLogo} disabledProps={["description"]} />
+              </div>
+            )
+          })
+        }
+      </div>
+    </Container>
+  );
+}
+
+export default Experiences;
---
+++ src/components/Experiences/experienceLogos.js
@@ -0,0 +1,13 @@
+import kylasLogo from "../../Assets/experiences/kylas.jpeg";
+import qblocksLogo from "../../Assets/experiences/qblocks.jpeg";
+import samagraLogo from "../../Assets/experiences/samagra.jpeg";
+import tenlabsLogo from "../../Assets/experiences/tenlabs.jpeg";
+import mozillaLogo from "../../Assets/experiences/mozilla.jpeg";
+
+export {
+    kylasLogo,
+    qblocksLogo,
+    samagraLogo,
+    tenlabsLogo,
+    mozillaLogo
+}
