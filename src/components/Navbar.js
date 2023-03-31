de64d412f3338e850cdfd6a0627e5c5818c0cef3
amdhanwate <iamdhanwate@gmail.com>
2023-03-31 16:04:10 +0530

    cleaned code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import {
-  AiFillStar, AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser, AiOutlineMenu
+  AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser
 } from "react-icons/ai";
-import { RiArrowDropDownLine } from "react-icons/ri";
-import { CgGitFork } from "react-icons/cg";
 import { ImLinkedin, ImBooks } from "react-icons/im";
-import { GiVideoConference } from "react-icons/gi";
-import { GrCertificate } from "react-icons/gr";
 import { MdOutlineWorkOutline } from "react-icons/md"
 import { Link } from "react-router-dom";
-import { Dropdown } from "react-bootstrap";
 
 
 function NavBar() {
@@ -112,25 +106,6 @@ function NavBar() {
                 <ImLinkedin style={{ marginBottom: "2px" }} /> Linkedin
               </Nav.Link>
             </Nav.Item>
-
-            {/* <Nav.Item className="fork-btn">
-              <Dropdown className="text-white" align="end" drop="drop-centered">
-                <Dropdown.Toggle as={"span"} className="hidden-dropdown-arrow">
-                   <AiOutlineMenu style={{ marginBottom: "2px" }} />
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                <Dropdown.Item as={Link} to="/education" onClick={() => updateExpanded(false)}>
-                    <ImBooks style={{ marginBottom: "2px" }} /> Education
-                  </Dropdown.Item>
-                  <Dropdown.Item as={Link} to="/education#certifications" onClick={() => updateExpanded(false)}>
-                    <GrCertificate style={{ marginBottom: "2px" }} /> Certifications
-                  </Dropdown.Item>
-                  <Dropdown.Item as={Link} to="/education#conferences" onClick={() => updateExpanded(false)}>
-                    <GiVideoConference style={{ marginBottom: "2px" }} /> Conferences
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown >
-            </Nav.Item> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
